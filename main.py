461b2a08633d7fde6d1b4025dc1374905258fc3a
ramdamuser <ramdamuser@gmail.com>
2023-12-29 13:07:11 +1100

    Enable users to see button if they are co-admin

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ class BlogPost(db.Model):
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
-    img_url = db.Column(db.Text(500), nullable=False)
+    img_url = db.Column(db.Text, nullable=False)
     comments = relationship("Comment", back_populates="parent_post")
 
 
