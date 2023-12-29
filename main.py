32f37b0128c929177dade8671b00c1fab837655a
ramdamuser <ramdamuser@gmail.com>
2023-12-29 11:16:04 +1100

    Initial Commit

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def register():
             flash("You've already signed up with that email, log in instead!")
             return redirect(url_for("login"))
         password = generate_password_hash(form.password.data, method="pbkdf2:sha256", salt_length=8)
-        new_user = User(email=form.email.data, password=password, name=form.name.data)
+        new_user = User(email=form.email.data, password=password, name=form.name.data, is_co_admin=False)
         db.session.add(new_user)
         db.session.commit()
         login_user(new_user)
